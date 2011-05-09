215bd8c6dbc0a4fd799b162104ccb56dceadeba3
dingqiu <dingqiu@taobao.com>
2011-05-09 22:16:17 +0800

    添加嵌入静态资源到模块的

--- demo/d.js
+++ demo/d.js
@@ -1,5 +1,17 @@
-module('d', 'c', function () {
+module('d', ['c'], function (mod) {
+    var res = mod.embed('p1.css');
+
     return {
         a: 'd'
     };
-});
+}, {embed: ['p1.css', 'p2.less']});
+
+--to->
+
+module('d', ['c'], function (mod) {
+    var res = mod.embed('p1.css');
+
+    return {
+        a: 'd'
+    };
+}, {_embed: {'p1.css': 'ssdsf\n\'\n"', 'p2.less': null, 'p3.tmpl': null}});
