8bd6c3fe49c0853d83c7ead66760feb7a2a1c07b
dingqiu <dingqiu@taobao.com>
2011-05-12 11:20:04 +0800

    重排 miniajax

--- src/loader.js
+++ src/loader.js
@@ -608,11 +608,43 @@ modified
 */
 
     var ajax={};
-    ajax.x=function(){try{return new ActiveXObject('Msxml2.XMLHTTP')}catch(e){try{return new ActiveXObject('Microsoft.XMLHTTP')}catch(e){return new XMLHttpRequest()}}};
-    ajax.send=function(u,f,m,a){var x=ajax.x();x.open(m,u,true);x.onreadystatechange=function(){if(x.readyState==4)f(x.responseText, x)};if(m=='POST')x.setRequestHeader('Content-type','application/x-www-form-urlencoded');x.send(a)};
-    ajax.get=function(url,func){ajax.send(url,func,'GET', null)};
-    ajax.syncget=function(url){var x=ajax.x();x.open('GET',url,false);x.send(null);return x.responseText};
-    ajax.post=function(url,func,args){ajax.send(url,func,'POST',args)};
+
+    ajax.x = function () {
+        try {
+            return new ActiveXObject('Msxml2.XMLHTTP');
+        } catch (e) {
+            try {
+                return new ActiveXObject('Microsoft.XMLHTTP');
+            } catch (e) {
+                return new XMLHttpRequest();
+            }
+        }
+    };
+    ajax.send = function (u, f, m, a) {
+        var x = ajax.x();
+        x.open(m, u, true);
+        x.onreadystatechange = function () {
+            if (x.readyState == 4) {
+                f(x.responseText, x);
+            }
+        };
+        if (m == 'POST') {
+            x.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+        }
+        x.send(a);
+    };
+    ajax.get = function (url, func) {
+        ajax.send(url, func, 'GET', null);
+    };
+    ajax.syncget = function (url) {
+        var x = ajax.x();
+        x.open('GET', url, false);
+        x.send(null);
+        return x.responseText;
+    };
+    ajax.post = function (url, func, args) {
+        ajax.send(url, func, 'POST', args);
+    };
 
     return ajax;
 });
